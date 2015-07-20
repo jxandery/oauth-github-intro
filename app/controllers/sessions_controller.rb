c25ea94bb1f25893d7b42e55f52b783996d8afc8
Jack Yeh <jack.yeh@gmail.com>
2015-07-20 10:58:14 -0600

    add logout functionality: - add destroy method to the controller - add a logout path to the routes - replace the empty string to logout_path route in the view
    move the oauth method into a private method
       + oauth is performing the same function as strong params
       + you don't want to give users or visitors to your api access to the authorization information that the github/api provider is returning to you

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,15 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  private
+
   def oauth
     request.env['omniauth.auth']
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/create'
   get '/auth/github', as: :login
+  delete '/logout', as: :logout, to: 'sessions#destroy'
 
   get '/auth/github/callback', to: 'sessions#create'
   get  '/dashboard', to: 'dashboard#show'
