ff45aec7d8906727a4a052fb3d8c11e9ca9ce531
Jack Yeh <jack.yeh@gmail.com>
2015-07-20 10:34:41 -0600

    refactor: routes and view - create a custom path so that you can use the helper path method in the views instead of the url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/create'
+  get '/auth/github', as: :login
 
   get '/auth/github/callback', to: 'sessions#create'
   get  '/dashboard', to: 'dashboard#show'
